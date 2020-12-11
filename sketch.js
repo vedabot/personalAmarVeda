6f33940d10862bc2b3eaf996aff0225abb2e1723
vedabot <75852609+vedabot@users.noreply.github.com>
2020-12-11 23:37:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ function setup() {
   
    lovesprite = createSprite(300,50,75,40);
   lovesprite.addImage(loveImage);
-  lovesprite.scale=1;
+  lovesprite.scale=0.3;
   lovesprite.visible = false;
 }
 function preload(){
