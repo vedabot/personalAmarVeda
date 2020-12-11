c51de55a018e242ca816e9fde7a9ed638563808f
vedabot <75852609+vedabot@users.noreply.github.com>
2020-12-11 22:57:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+function setup() {
+  createCanvas(600, 300);
+  
+  amarsprite = createSprite(50,150,20,50);
+  amarsprite.addImage(amarImage);
+  vedasprite = createSprite(550,150,20,50);
+   vedasprite.addImage(vedaImage);
+  
+   love = createSprite(300,50,75,40);
+  love.addImage(loveImage);
+  love.visible = false;
+}
+function preload(){
+
+  amarImage = loadImage("amar.png");
+  
+  vedaImage = loadImage("veda.png");
+ loveImage = loadImage("love.png");
+  
+}
+
+function draw() {
+  background("white");
+   
+  
+   clicksprite = createSprite(300,240,75,40);
+   text("Click on the above box",240,280);
+   
+    if(mousePressedOver(clicksprite)) {
+      amarsprite.velocityX=3;
+      vedasprite.velocityX=-3;
+    }
+  if(amarsprite.isTouching(vedasprite)) {
+      amarsprite.velocityX=0;
+      vedasprite.velocityX=0;
+    love.visible = true;
+    }
+   
+  
+  drawSprites();
+}
